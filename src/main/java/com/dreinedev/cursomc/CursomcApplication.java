87b0b456c58e1b03f295f0307d327222c713b8ca
Dreine Antonio <dreineantonio@hotmail.com>
2024-06-11 19:01:36 -0300

    Criando repository e service para Categoria

--- src/main/java/com/dreinedev/cursomc/CursomcApplication.java
+++ src/main/java/com/dreinedev/cursomc/CursomcApplication.java
@@ -2,8 +2,11 @@ package com.dreinedev.cursomc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
+@ComponentScan(value = "com.dreinedev.controller")
 public class CursomcApplication {
 
 	public static void main(String[] args) {
